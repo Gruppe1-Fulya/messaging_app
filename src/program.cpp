cbd3afd8f3728f0e4007604b357066ff390082a3
enaciodabasi <enaciodabasi@outlook.com>
2023-03-18 23:44:08 +0300

    Created INF202 Project Messaging-Application.

---
+++ src/program.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file program.cpp
+ * @author Eren Naci Odabasi enaciodabasi@outlook.com
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int main(int argc, char** argv)
+{
+    
+}
